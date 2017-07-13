31dc22de24ae408fbd58516c30c918d87d65fdc3
Brannon Dorsey <brannon@brannondorsey.com>
2017-07-13 16:51:28 -0500

    add UTF8 encoding

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ for FILE in $(ls markdown/*.md) ; do
 	OUTPUT="www/$(echo "$FILE" | sed -e "s/markdown\///" | sed -e "s/\.md$/.html/")"
 	mkdir -p $(dirname "$OUTPUT")
 	# convert to markdown, adding the "markdown-body" class for styling
-	markdown "$FILE" --flavor gfm --stylesheet css/github-markdown.css | sed -e "s/<body>/<body class='markdown-body'>/" > "$OUTPUT"
+	markdown "$FILE" --flavor gfm --stylesheet css/github-markdown.css | \
+	sed -e "s/<body>/<body class='markdown-body'>/" | sed -e "s/<head>/<head><meta charset='utf-8'>/" > "$OUTPUT"
 done
 
 # open w/ firefox
