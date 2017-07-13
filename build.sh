1e5f3afd4253e49ad171a7b5bce51aa16655c214
Brannon Dorsey <brannon@brannondorsey.com>
2017-07-13 15:41:29 -0500

    Add README

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ for FILE in $(ls markdown/*.md) ; do
 	OUTPUT="www/$(echo "$FILE" | sed -e "s/markdown\///" | sed -e "s/\.md$/.html/")"
 	mkdir -p $(dirname "$OUTPUT")
 	# convert to markdown, adding the "markdown-body" class for styling
-	markdown "$FILE" --stylesheet css/github-markdown.css | sed -e "s/<body>/<body class='markdown-body'>/" > "$OUTPUT"
+	markdown "$FILE" --flavor gfm --stylesheet css/github-markdown.css | sed -e "s/<body>/<body class='markdown-body'>/" > "$OUTPUT"
 done
 
 # open w/ firefox
